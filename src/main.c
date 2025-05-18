0d0ce125482d8f6600e9218ecb02e925aa702d7a
f3fe-hash <f3fe5.unhash@gmail.com>
2025-05-18 17:28:22 -0400

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,5 +3,10 @@
 int main(int argc, char** argv)
 {
     printf("Hello from dummy package!\n");
+    printf("Args: \n");
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s\n", argv[i]);
+    }
     return 0;
 }
